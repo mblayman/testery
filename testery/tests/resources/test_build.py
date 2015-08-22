65c1f4a378a1c534ab5e100709a63c96789136b0
Matt Layman <matthewlayman@gmail.com>
2015-08-22 13:18:52 -0400

    Test build collection status.

--- testery/tests/resources/test_build.py
+++ testery/tests/resources/test_build.py
@@ -1,7 +1,17 @@
+import falcon
+
+from testery.resources.build import BuildCollection
 from testery.tests import TestCase
 
 
 class TestBuildCollection(TestCase):
 
+    def _make_one(self):
+        return BuildCollection()
+
     def test_status_ok(self):
-        pass
+        req = self.factory.make_req()
+        resp = self.factory.make_resp()
+        resource = self._make_one()
+        resource.on_get(req, resp)
+        self.assertEqual(falcon.HTTP_200, resp.status)
