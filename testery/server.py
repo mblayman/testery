ff1b90257ead84613f829ea163444446fcfa59a4
Matt Layman <matthewlayman@gmail.com>
2015-08-24 14:48:15 -0400

    Setup tests from  a configuration file.
    Initially, it seemed like this would be overkill
    for running tests. Then I realized that, in order
    to test main, settings must come a file. Without that,
    I would never be able to get coverage for the line that
    creates the engine.

--- testery/server.py
+++ testery/server.py
@@ -1,5 +1,7 @@
 import falcon
+import sqlalchemy
 
+from testery import db
 from testery.resources import build
 
 
@@ -9,9 +11,25 @@ def main(global_config, **settings):
     This method signature follows the paste.app_factory protocol
     for Paste Deploy.
     """
-    api = falcon.API()
+    configure_db(settings)
+    api = make_api()
+    set_routes(api)
+    return api
 
+
+def configure_db(settings):
+    engine = sqlalchemy.engine_from_config(settings)
+    db.Session.configure(bind=engine)
+    db.Base.metadata.bind = engine
+
+
+def make_api():
+    api = falcon.API()
+    # TODO: use custom request
+    # TODO: use middleware
+    return api
+
+
+def set_routes(api):
     build_collection = build.BuildCollection()
     api.add_route('/builds', build_collection)
-
-    return api
--- testery/tests/__init__.py
+++ testery/tests/__init__.py
@@ -1,13 +1,20 @@
-import sqlalchemy
+import os
 
-from .. import db, models
+from paste.deploy.loadwsgi import appconfig
+
+from .. import models, server
 from .testcase import TestCase  # noqa
 
 
 def setup():
-    """Prepare to execute tests by setting up the database."""
-    # Set echo to True to view SQL.
-    engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
-    db.Session.configure(bind=engine)
+    """Prepare to execute tests by setting up the database.
+
+    main does the db configuration. Then the tables need to be created.
+    """
+    here = os.path.dirname(__file__)
+    ini_path = os.path.abspath(os.path.join(here, '..', '..', 'test.ini'))
+    settings = appconfig('config:' + ini_path)
+    server.main(global_config={}, **settings)
+
     # Use Base from models to get the benefit of declaring all models.
-    models.Base.metadata.create_all(engine)
+    models.Base.metadata.create_all()
