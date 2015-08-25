61c72dee06361bccc29d8f9142fdb0d8834498d0
Matt Layman <matthewlayman@gmail.com>
2015-08-24 22:31:58 -0400

    Add BuildCollection.on_get (crudely).

--- testery/resources/build.py
+++ testery/resources/build.py
@@ -1,8 +1,19 @@
+import json
+
 import falcon
+
+from testery.models import Build
 
 
 class BuildCollection(object):
     """A set of summary information for builds."""
 
     def on_get(self, req, resp):
+        # TODO: test configuration files
+        # TODO: make some marshalling stuff
+        builds = req.session.query(Build).all()
+        marshalled = {'builds': []}
+        for build in builds:
+            marshalled['builds'].append({'id': build.id})
+        resp.body = json.dumps(marshalled)
         resp.status = falcon.HTTP_200
--- testery/tests/resources/test_build.py
+++ testery/tests/resources/test_build.py
@@ -1,3 +1,5 @@
+import json
+
 import falcon
 
 from testery.resources.build import BuildCollection
@@ -15,3 +17,21 @@ class TestBuildCollection(TestCase):
         resource = self._make_one()
         resource.on_get(req, resp)
         self.assertEqual(falcon.HTTP_200, resp.status)
+
+    def test_get(self):
+        build_1 = self.factory.make_build()
+        build_2 = self.factory.make_build()
+        req = self.factory.make_req()
+        resp = self.factory.make_resp()
+        resource = self._make_one()
+
+        resource.on_get(req, resp)
+
+        expected = {
+            'builds': [{
+                'id': build_1.id,
+            }, {
+                'id': build_2.id,
+            }]
+        }
+        self.assertEqual(expected, json.loads(resp.body))
