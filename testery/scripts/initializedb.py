851fc82a73b959368fbb3ef76b10149c5d04d76b
Matt Layman <matthewlayman@gmail.com>
2015-08-26 23:12:40 -0400

    Add some sample data into the initdb command.

--- testery/scripts/initializedb.py
+++ testery/scripts/initializedb.py
@@ -2,7 +2,7 @@ import os
 
 from paste.deploy.loadwsgi import appconfig
 
-from testery import models, server
+from testery import db, models, server
 
 
 def main():
@@ -16,6 +16,13 @@ def main():
 
     # # Use Base from models to get the benefit of declaring all models.
     models.Base.metadata.create_all()
+
+    session = db.Session()
+    session.add(models.Build(passes=42, fails=4))
+    session.add(models.Build(passes=52, fails=2))
+    session.add(models.Build(passes=62, fails=1))
+    session.commit()
+
 
 if __name__ == '__main__':
     main()
